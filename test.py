0bd4541c63536bf0837e9037d7d1e60bfbfbf93d
callac2 <callac2@nycstudents.net>
2026-02-04 09:22:30 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,58 @@ y = float(3)
 print(x,y)
 
  """
-
-values = [1,2.23,5,7,2,30,15]
+""" values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
-    print(i)
+    print(i) """
+""" 
+def discount(age, Member, Resident):
+    
+    if age <= 12 or age >= 65:
+        print("Discount applicable!")
+
+    elif Member == True:
+        print("Discount applicable!")
+    elif Resident == True:
+        print("Discount applicable!")
+    else:
+        print("No Discount Applicable, Sorry!")
+discount(11, False, False) """
+
+
+""" values = [1,2.23,6,7,2,30,15]
+print(values)
+print(values[1])
+print(values[3])
+         """
+
+""" "test"
+["t","e","s","t"]
+print(["t","e","s","t"][1]) """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[1]
+print(y)
+print(z) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+""" 
+x = "test"
+y = "tEsT"
+print(f"yay {y}")
+ """
+
+""" temp = 68
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+
